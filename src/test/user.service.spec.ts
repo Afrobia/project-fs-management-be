93e2cc2793daa3d985b700e5dd31ef2a734d11f7
Beatriz Santana <afrobia.dev@gmail.com>
2025-11-27 10:13:26 -0300

    add

---
+++ src/test/user.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../core/user/user.service';
+import { UserRepository } from '../core/user/user.repository';
+
+describe('UserService', () => {
+  let service: UserService;
+  let repository: UserRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+            deleteById: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    repository = module.get<UserRepository>(UserRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
